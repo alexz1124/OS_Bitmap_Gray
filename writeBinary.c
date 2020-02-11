935b859eb4494f7e90bcce98419da214ac4cd3b8
kanuntd <kanun.natthanan@gmail.com>
2020-02-11 16:53:05 +0700

    -

--- writeBinary.c
+++ writeBinary.c
@@ -1,4 +1,3 @@
-
 #include<stdio.h>
 
 	/* Our structure */
@@ -13,24 +12,17 @@
 		FILE *ptr_myfile;
 		struct rec my_record;
 
-		ptr_myfile=fopen("test.bin","rb");
+		ptr_myfile=fopen("test.bin","wb");
 		if (!ptr_myfile)
 		{
 			printf("Unable to open file!");
 			return 1;
 		}
-
-		fseek(ptr_myfile, sizeof(struct rec), SEEK_END);
-		rewind(ptr_myfile);
-
 		for ( counter=1; counter <= 10; counter++)
 		{
-			fread(&my_record,sizeof(struct rec),1,ptr_myfile);
-			printf("%d\n",my_record.x);
+			my_record.x= counter;
+			fwrite(&my_record, sizeof(struct rec), 1, ptr_myfile);
 		}
 		fclose(ptr_myfile);
 		return 0;
 	}
-
-
-
