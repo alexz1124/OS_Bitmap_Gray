c05d45ddb6f6c587dcd115eb87c8f5e7923f6f9f
kanuntd <kanun.natthanan@gmail.com>
2020-02-12 16:56:10 +0700

    Ff

--- childTest.c
+++ childTest.c
@@ -3,37 +3,60 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main(void){
+int main(void)
+{
     int i, tmp;
     pid_t pid[4];
-    int round=0;
+    int round = 0;
 
-    for (i=0;i<4;i++) {
+    for (i = 0; i < 4; i++)
+    {
         pid[i] = fork();
-        if (pid[i] == 0) {
+        if (pid[i] == 0)
+        {
             break;
         }
     }
 
-    if (pid[0] != 0 && pid[1] != 0 && pid[2] != 0 && pid[3] != 0) {
+    if (pid[0] != 0 && pid[1] != 0 && pid[2] != 0 && pid[3] != 0)
+    {
         // That's the father, it waits for all the childs
-        printf("I'm the father [pid: %d, ppid: %d]\n",getpid(),getppid());
-        for(i=0;i<4;i++) {
+        printf("I'm the father [pid: %d, ppid: %d]\n", getpid(), getppid());
+        for (i = 0; i < 4; i++)
+        {
             wait(&tmp);
         }
-    } else {
-        if(getpid()==getppid()+1){
-             printf("I'm one of the children [pid: %d, ppid: %d] round 1\n",getpid(),getppid());
-        }else if(getpid()==getppid()+2){
-            printf("I'm one of the children [pid: %d, ppid: %d] round 2\n",getpid(),getppid());
-        }else if(getpid()==getppid()+3){
-            printf("I'm one of the children [pid: %d, ppid: %d] round 3\n",getpid(),getppid());
-        }else if(getpid()==getppid()+4){
-            printf("I'm one of the children [pid: %d, ppid: %d] round 4\n",getpid(),getppid());
+    }
+    else
+    {
+        if (getpid() == getppid() + 1)
+        {
+            printf("I'm one of the children [pid: %d, ppid: %d] round 1\n", getpid(), getppid());
+
+            printf("I'm one of the children [pid: %d, ppid: %d] round 1\n", getpid(), getppid());
+
+            printf("I'm one of the children [pid: %d, ppid: %d] round 1\n", getpid(), getppid());
+
+            printf("I'm one of the children [pid: %d, ppid: %d] round 1\n", getpid(), getppid());
+
+            printf("I'm one of the children [pid: %d, ppid: %d] round 1\n", getpid(), getppid());
+
+            printf("I'm one of the children [pid: %d, ppid: %d] round 1\n", getpid(), getppid());
         }
-        
+        else if (getpid() == getppid() + 2)
+        {
+            printf("I'm one of the children [pid: %d, ppid: %d] round 2\n", getpid(), getppid());
+        }
+        else if (getpid() == getppid() + 3)
+        {
+            printf("I'm one of the children [pid: %d, ppid: %d] round 3\n", getpid(), getppid());
+        }
+        else if (getpid() == getppid() + 4)
+        {
+            printf("I'm one of the children [pid: %d, ppid: %d] round 4\n", getpid(), getppid());
+        }
+
         // That's the child, it print its pid, its parent pid and exits.
-       
     }
 
     return 0;
