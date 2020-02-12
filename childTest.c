ccdec14611e0465ae49466e9cf539118fa73e16f
kanuntd <kanun.natthanan@gmail.com>
2020-02-12 15:34:39 +0700

    tess

---
+++ childTest.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(void){
+    int i, tmp;
+    pid_t pid[4];
+    int round=0;
+
+    for (i=0;i<4;i++) {
+        pid[i] = fork();
+        if (pid[i] == 0) {
+            break;
+        }
+    }
+
+    if (pid[0] != 0 && pid[1] != 0 && pid[2] != 0 && pid[3] != 0) {
+        // That's the father, it waits for all the childs
+        printf("I'm the father [pid: %d, ppid: %d]\n",getpid(),getppid());
+        for(i=0;i<4;i++) {
+            wait(&tmp);
+        }
+    } else {
+         printf("I'm one of the children [pid: %d, ppid: %d] round %d\n",getpid(),getppid(),round);
+        round++;
+        // That's the child, it print its pid, its parent pid and exits.
+       
+    }
+
+    return 0;
+}
